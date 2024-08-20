fa24e16922c26700ccc3f05ae6acee686a647dd4
eylon <eylont@comm-it.com>
2024-08-20 12:24:27 +0300

    terraform fmt cleanup

--- tfstate-resources/variables.tf
+++ tfstate-resources/variables.tf
@@ -4,11 +4,11 @@ variable "region" {
 }
 
 variable "s3_bucket_name" {
-    default = "bucket-terraform-state"
-    description = "The name of the tfstate S3 bucket"
+  default     = "bucket-terraform-state"
+  description = "The name of the tfstate S3 bucket"
 }
 
 variable "dynamodb_table_name" {
-    default = "table-terraform-state-locks"
-    description = "The name of the dynamodb table for state locking"
+  default     = "table-terraform-state-locks"
+  description = "The name of the dynamodb table for state locking"
 }
