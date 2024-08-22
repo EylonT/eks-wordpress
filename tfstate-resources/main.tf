281000f69b6fcfba270f3d102f1af8b0e2e3d249
eylon <eylont@comm-it.com>
2024-08-22 13:51:04 +0300

    Added PITR for DynamoDB table

--- tfstate-resources/main.tf
+++ tfstate-resources/main.tf
@@ -43,6 +43,9 @@ resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+  point_in_time_recovery {
+    enabled = true
+  }
 
   attribute {
     name = "LockID"
