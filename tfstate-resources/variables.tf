103f93fff36d4ba26a87fd2021d6e0bfe72e4824
eylon <eylont@comm-it.com>
2024-08-22 13:43:39 +0300

    Added tflint recommnedations

--- tfstate-resources/variables.tf
+++ tfstate-resources/variables.tf
@@ -1,14 +1,17 @@
 variable "region" {
   default     = "us-east-1"
   description = "AWS region"
+  type        = string
 }
 
 variable "s3_bucket_name" {
   default     = "bucket-terraform-state"
   description = "The name of the tfstate S3 bucket"
+  type        = string
 }
 
 variable "dynamodb_table_name" {
   default     = "table-terraform-state-locks"
   description = "The name of the dynamodb table for state locking"
+  type        = string
 }
